1c522e6ff29cc578f72f65a28d767b8f372d367e
Tavilo Breno <breno_wk2@hotmail.com>
2024-03-08 15:55:28 -0300

    Implementando sistema JWT

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,10 +1,32 @@
-// controllers/authController.ts
-import { Request, Response } from "express";
-import { generateToken } from "../utils/authUtils";
+// middlewares/authentication.ts
+import { Request, Response, NextFunction } from "express";
+import { verifyToken } from "../utils/authUtils";
+import { JWTPayload } from "../interfaces/JWTPayload";
 
-export const login = (req: Request, res: Response) => {
-  // Lógica de autenticação...
-  const userId = "idDoUsuario"; // Supondo que você tenha o ID do usuário após a autenticação
-  const token = generateToken(userId);
-  res.json({ token });
+export const authenticateToken = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    // Obtém o token do cabeçalho Authorization
+    const token = req.headers.authorization?.split(" ")[1];
+    if (!token) {
+      return res
+        .status(401)
+        .json({ error: "Token de autenticação não fornecido" });
+    }
+
+    // Verifica se o token é válido
+    const decoded = verifyToken(token) as JWTPayload;
+
+    // Adiciona o ID do usuário decodificado ao objeto de solicitação
+    req.userId = decoded.userId;
+
+    // Chama o próximo middleware
+    next();
+  } catch (error) {
+    console.error("Erro ao autenticar token:", error);
+    res.status(403).json({ error: "Falha na autenticação. Token inválido" });
+  }
 };
